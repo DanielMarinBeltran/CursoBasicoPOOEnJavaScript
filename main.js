38ef9894ceaa4ff5ec65ebdaa03f7b55fa6a740b
Daniel Marin <danielmarin998@hotmail.com>
2021-08-16 15:21:18 -0500

    Se vio un claro ejemplo de la importancia de POO

--- main.js
+++ main.js
@@ -1,60 +1,66 @@
-const natalia = {
-    name: "Natalia",
-    age: 20,
-    cursosAprovados: [
-        "Curso Definitivo de HTML y CSS",
-        "Curso Practico de HTML y CSS",
-    ],
-    aprobarCurso(nuevoCursito){
-        this.cursosAprovados.push(nuevoCursito);
-    },
-};
-
-// Hacer Natalia apruebe otro curso
-// natalia.cursosAprovados.push("Curso de Responsive Design");
-
-function Student (name, age, cursosAprobados){
-    this.name = name;
-    this.age = age;
-    this.cursosAprobados = cursosAprobados;
-    // this.aprobarCurso = function (nuevoCursito){
-    //     this.cursosAprobados.push(nuevoCursito);
-    // } Esta es una forma de trabajar con el prototipo, pero ya vamos a ver una forma mas interesante.
-};
-
-Student.prototype.aprobarCurso = function (nuevoCursito){
-    this.cursosAprobados.push(nuevoCursito);
-};
-const juanita = new Student(
-    "Juanita Alejandra",
-    15,
-    [
-        "Curso de CSS y HTML",
-        "Curso de preprocesadores",
-    ]
-); 
-
-//Prototipos con la sintacis de clases
-
-class Student2 {
+// Inicia reto de la clase.
+class LearningPath {
     constructor({
         name,
-        age,
-        cursosAprobados = [],
-        email
+        courses = [],
+    }){
+        this.name = name;
+        this.courses = courses;
+    }
+};
+const escuelaWeb = new LearningPath({
+    name: "Escuela de Desarrollo Web",
+    courses: [
+        "HTML Y CSS",
+        "CursoJS",
+        "CursoReact",
+        "CursoNextJS",
+    ]
+});
+const escuelaData = new LearningPath({
+    name: "Escuela de Data Science",
+    courses: [
+        "cursoPython",
+        "CursoMatematicas",
+        "CursoExcel",
+    ],
+  });
+// Finaliza reto de la clase.
+
+class Student {
+    constructor({
+        name,
+        email,
+        usename,
+        twitter = undefined,
+        instagram = undefined,
+        facebook = undefined,
+        approvedCourses = [],
+        learningPaths = [],
     }){
         this.name = name;
         this.email = email;
-        this.age = age;
-        this.cursosAprobados = cursosAprobados;
-    }
-    aprobarCurso(nuevoCursito){
-        this.cursosAprobados.push(nuevoCursito);
+        this.usename = usename;
+        this.socialMedia = {
+            twitter,
+            instagram,
+            facebook,
+        };
+        this.approvedCourses = approvedCourses;
+        this.learningPaths = learningPaths;
     }
 }
 
-const miguelito = new Student2({
-    email : "miguel@gmail.com",
-    name: "Miguel",
-    age: 28,
+const juan2 = new Student({
+    name: "DanielMB",
+    username: "daniel2015",
+    email: "daniel2015@hotmail.com",
+    twitter: "danielmb"
+});
+
+const miguelito2 = new Student({
+    name: "Miguelito",
+    username: "miguelitofeliz",
+    email: "miguel@hotmail.com",
+    instagram: "miguel_rc"
 });
